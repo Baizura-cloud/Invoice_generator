38a5aefb3f606fee1c68d422af31988516e19afc
baizura <omakase.software@gmail.com>
2022-10-13 11:53:31 +0800

    Embeded Material Table + styling

--- src/file/table.js
+++ src/file/table.js
@@ -1,54 +1,79 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import { forwardRef } from 'react';
+import MaterialTable from 'material-table';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+import AddBox from '@material-ui/icons/AddBox';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Check from '@material-ui/icons/Check';
+import Clear from '@material-ui/icons/Clear';
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  
-];
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} style={{ fontSize: 18 }} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+};
+
 
 export default function TableForm() {
+  const { useState } = React;
+
+  const [columns, setColumns] = useState([
+    { title: 'Item', field: 'item' },
+    { title: 'Qty', field: 'qty', type: 'numeric' },
+    { title: 'Rate', field: 'rate', type: 'numeric' },
+    { title: 'Amount', field: 'amount', type: 'numeric' },
+  ]);
+
+  const [data, setData] = useState([
+   
+  ]);
+
   return (
-    <TableContainer >
-      <Table size="small" sx={{ minWidth: 650 }} >
-        <TableHead sx={{
-      "& th": {
-        backgroundColor: "#6fd5f1"
-      }
-    }}>
-          <TableRow>
-            <TableCell>Item</TableCell>
-            <TableCell align="right">Qty</TableCell>
-            <TableCell align="right">Rate</TableCell>
-            <TableCell align="right">Amount</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+    <MaterialTable
+      title=""
+      columns={columns}
+      icons={tableIcons}
+      data={data}
+      components={{
+        Container: props => <Paper {...props} elevation={0} />
+      }}
+      options={{
+        paging: false,
+        search: false,
+        sorting: false,
+        headerStyle: {
+          backgroundColor: '#6fd5f1',
+          color: '#000000'
+        },
+        rowStyle: {
+          fontSize: 14,
+        },
+
+      }}
+      editable={{
+        onRowAdd: newData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              setData([...data, newData]);
+
+              resolve();
+            }, 1000)
+          }),
+        onRowDelete: oldData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataDelete = [...data];
+              const index = oldData.tableData.id;
+              dataDelete.splice(index, 1);
+              setData([...dataDelete]);
+
+              resolve()
+            }, 1000)
+          }),
+      }}
+    />
+  )
 }
+
